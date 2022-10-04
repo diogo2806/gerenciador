78fc34946372b36eaf457703f819cd7c5d2e85a1
diogo <diogo@DESKTOP-HOL39IU>
2022-10-03 21:49:58 -0300

    atualização da classe OiMundoServlet

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException  {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset='UTF-8'>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("Olá Mundo 2.0!");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+}
