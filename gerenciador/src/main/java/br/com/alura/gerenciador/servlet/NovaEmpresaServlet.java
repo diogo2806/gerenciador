26b9c18a953728a80b268a5278d344514470b7aa
diogo <diogo@DESKTOP-HOL39IU>
2022-10-04 22:09:26 -0300

    novas classes

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException  {
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html><head><meta charset='UTF-8'></head><body>Olá Mundo 3.0!</body></html>");
+		
+	}
+}
